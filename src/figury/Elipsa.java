a14419509d61bd083f874cdf91d6a9e5a7137075
markotny <markotny97@gmail.com>
2017-11-23 14:13:06 +0100

    uzupełniono o klasy kwadrat i elipsa

--- src/figury/Elipsa.java
+++ src/figury/Elipsa.java
@@ -3,13 +3,13 @@ package figury;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
-import java.awt.geom.Rectangle2D;
+import java.awt.geom.Ellipse2D;
 
-public class Kwadrat  extends Figura {
+public class Elipsa extends Figura {
 
-    public Kwadrat(Graphics2D buffer, int delay, int width, int height) {
+    public Elipsa(Graphics2D buffer, int delay, int width, int height) {
         super(buffer,delay,width,height);
-        shape = new Rectangle2D.Float(0,0,10,10);
+        shape = new Ellipse2D.Float(0,0,10,10);
         aft = new AffineTransform();
         area = new Area(shape);
     }
